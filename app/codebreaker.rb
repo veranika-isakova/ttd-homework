16b17dc26c7e5702f7f5aad998ff7f1725fed221
VERANIKA <veranika.isakova89@gmail.com>
2017-11-26 12:57:40 +0100

    Improve C:An exact match sends a mark with '+'

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -16,18 +16,20 @@ class Codebreaker
     def guess(input)
       # Make sure to replace next line with the actual implemented marking algorithm,
       # using the @secret_number
-      result = ""
+      result_number_match = ""
+      result_exact_match = ""
 
       @secret_number.split("").each do |number|
         if input.include? number
-          result << ('-')
+          result_number_match << ('-')
         end
         if @secret_number.index(number) == input.index(number)
-          result << ('+')
+          result_exact_match << ('+')
         end
       end
 
-      output.puts(result)
+      output.puts(result_number_match)
+      output.puts(result_exact_match)
 
       if input.length == 4
         output.puts ""
