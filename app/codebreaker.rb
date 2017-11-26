257e54c6e3a272f56d2583f4c49eaca70d7d7508
VERANIKA <veranika.isakova89@gmail.com>
2017-11-26 16:55:40 +0100

    [D] with 2 matches - final code

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -14,22 +14,21 @@ class Codebreaker
     end
 
     def guess(input)
-      # Make sure to replace next line with the actual implemented marking algorithm,
-      # using the @secret_number
+
       result_number_match = ""
       result_exact_match = ""
-
+      
       @secret_number.split("").each do |number|
-        if input.include? number
-          result_number_match << ('-')
-        end
         if @secret_number.index(number) == input.index(number)
           result_exact_match << ('+')
+        elsif input.include? number
+          result_number_match << ('-')
         end
       end
 
-      output.puts(result_number_match)
-      output.puts(result_exact_match)
+      common_result = result_exact_match + result_number_match
+
+      output.puts(common_result)
 
       if input.length == 4
         output.puts ""
